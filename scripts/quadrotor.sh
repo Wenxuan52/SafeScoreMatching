f13865e98ac929bbc8d2b0ab28b10786aacc8774
Wenxuan Yuan <wenxuan.yuan@qq.com>
2026-01-05 17:59:03 +0000

    update

--- scripts/quadrotor.sh
+++ scripts/quadrotor.sh
@@ -1,29 +1,29 @@
 #!/bin/bash -l
-#SBATCH --job-name=quadrotor
+#SBATCH --job-name=quadrotor_ssm
 #SBATCH --partition=root
 #SBATCH --qos=long
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=40G
 #SBATCH --time=24:00:00
-#SBATCH -e quadrotor.err
-#SBATCH -o quadrotor.out
+#SBATCH -e quadrotor_ssm.err
+#SBATCH -o quadrotor_ssm.out
 
 source /scratch_root/wy524/miniconda3/etc/profile.d/conda.sh
 conda activate jaxrl
 
 # cd examples
 
-# python examples/quadrotor/train_td3_quad2d_baseline.py \
+# python examples/quadrotor/train_td3_lag_quad2d_baseline.py \
 #   --env_name QuadrotorTracking2D-v0 \
 #   --seed 0 \
-#   --max_steps 2010000 \
+#   --max_steps 5010000 \
 #   --start_training 10000 \
-#   --eval_interval 50000 \
+#   --eval_interval 500000 \
 #   --eval_episodes 4 \
 #   --batch_size 256 \
 #   --utd_ratio 1 \
-#   --save_interval 50000 \
+#   --save_interval 500000 \
 #   --wandb False
 
 python examples/quadrotor/train_ssm_quad2d.py \
